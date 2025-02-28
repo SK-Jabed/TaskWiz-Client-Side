3b2f62968c8513e3e55b9ef2c9f245c7194b21f7
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-28 18:06:16 +0600

    Task Board Jsx Component Updated and Functionalities Done

--- src/pages/Dashboard/Statistics.jsx
+++ src/pages/Dashboard/Statistics.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Statistics = () => {
-    return (
-        <div>
-            All Statistics are here
-        </div>
-    );
+  return <div>All Statistics are here</div>;
 };
 
 export default Statistics;
